1fac7c7246269707f4a25f017be45a362120ea1b
vasil tatarov <vasil.tatarov@abv.bg>
2020-09-16 11:07:50 +0300

    Stack

---
+++ C# Advanced/Stacks and Queues/Lab/P03_SimpleCalculator/StartUp.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P03_SimpleCalculator
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            var input = Console.ReadLine().Split().Reverse();
+            var stack = new Stack<string>(input);
+
+            while (stack.Count > 1)
+            {
+                var firstNum = int.Parse(stack.Pop());
+                var sign = stack.Pop();
+                var secondNum = int.Parse(stack.Pop());
+
+                if (sign == "-")
+                {
+                    stack.Push((firstNum - secondNum).ToString());
+                }
+                else if (sign == "+")
+                {
+                    stack.Push((firstNum + secondNum).ToString());
+                }
+            }
+
+            Console.WriteLine(stack.Pop());
+        }
+    }
+}
---
+++ C# Advanced/Stacks and Queues/Lab/P04_MatchingBrackets/StartUp.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P04_MatchingBrackets
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            var expresion = Console.ReadLine();
+            var stack = new Stack<int>();
+
+            for (int i = 0; i < expresion.Length; i++)
+            {
+                if (expresion[i] == '(')
+                {
+                    stack.Push(i);
+                }
+                else if (expresion[i] == ')')
+                {
+                    var startIndex = stack.Pop();
+                    var currExpresion = expresion.Substring(startIndex, i - startIndex + 1);
+                    Console.WriteLine(currExpresion);
+                }
+            }
+        }
+    }
+}
