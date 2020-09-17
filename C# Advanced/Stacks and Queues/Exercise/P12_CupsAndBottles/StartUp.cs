f87f38f3812342a00f6dc6e93cd8f255c2ec2230
vasil tatarov <vasil.tatarov@abv.bg>
2020-09-18 00:22:36 +0300

    StackAndQueue

---
+++ C# Advanced/Stacks and Queues/Exercise/P12_CupsAndBottles/StartUp.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P12_CupsAndBottles
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            var cupsCapacity = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            var filledBottles = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            var cups = new Queue<int>(cupsCapacity);
+            var bottles = new Stack<int>(filledBottles);
+            int totalWastedWater = 0;
+
+            while (cups.Any() && bottles.Any())
+            {
+                var currCup = cups.Dequeue();
+
+                while (currCup > 0 && bottles.Any())
+                {
+                    currCup -= bottles.Pop();
+                }
+
+                totalWastedWater += Math.Abs(currCup);
+            }
+
+            if (cups.Count != 0)
+            {
+                Console.WriteLine($"Cups: {string.Join(" ", cups)}");
+            }
+            else if (bottles.Count != 0)
+            {
+                Console.WriteLine($"Bottles: {string.Join(" ", bottles)}");
+            }
+
+            Console.WriteLine($"Wasted litters of water: {totalWastedWater}");
+        }
+    }
+}
