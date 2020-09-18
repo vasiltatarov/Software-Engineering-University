11a053fd40ba4c74947f87315ec3ce1031b91401
vasil tatarov <vasil.tatarov@abv.bg>
2020-09-19 00:04:25 +0300

    Arrays

---
+++ C# Advanced/Multidimensional Arrays/Multidimensional-Arrays-Lab/06.JaggedArrayModification/StartUp.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Linq;
+
+namespace _06.JaggedArrayModification
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            var rows = int.Parse(Console.ReadLine());
+            var matrix = ReadElements(rows);
+
+            while (true)
+            {
+                var command = Console.ReadLine();
+
+                if (command == "END")
+                {
+                    break;
+                }
+
+                var args = command.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                var sign = args[0];
+                var row = int.Parse(args[1]);
+                var col = int.Parse(args[2]);
+                var value = int.Parse(args[3]);
+
+                if (row < 0 || row >= matrix.Length || col < 0 || col >= matrix[row].Length)
+                {
+                    Console.WriteLine("Invalid coordinates");
+                    continue;
+                }
+
+                if (sign == "Add")
+                {
+                    matrix[row][col] += value;
+                }
+                else if (sign == "Subtract")
+                {
+                    matrix[row][col] -= value;
+                }
+            }
+
+            PrintElements(matrix);
+        }
+
+        private static void PrintElements(int[][] matrix)
+        {
+            foreach (var row in matrix)
+            {
+                Console.WriteLine(string.Join(" ", row));
+            }
+        }
+
+        private static int[][] ReadElements(int rows)
+        {
+            var matrix = new int[rows][];
+
+            for (int row = 0; row < matrix.Length; row++)
+            {
+                var elements = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+                matrix[row] = new int[elements.Length];
+
+                for (int col = 0; col < matrix[row].Length; col++)
+                {
+                    matrix[row][col] = elements[col];
+                }
+            }
+
+            return matrix;
+        }
+    }
+}
---
+++ C# Advanced/Multidimensional Arrays/Multidimensional-Arrays-Lab/07.PascalTriangle/StartUp.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace _07.PascalTriangle
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var matrix = new long[n][];
+            var currentWidth = 1;
+
+            for (int row = 0; row < matrix.Length; row++)
+            {
+                matrix[row] = new long[currentWidth];
+                matrix[row][0] = 1;
+                matrix[row][matrix[row].Length - 1] = 1;
+                currentWidth++;
+
+                if (matrix[row].Length > 2)
+                {
+                    for (int col = 1; col < matrix[row].Length - 1; col++)
+                    {
+                        var firstNum = matrix[row - 1][col];
+                        var secondNum = matrix[row - 1][col - 1];
+                        matrix[row][col] = firstNum + secondNum;
+                    }
+                }
+            }
+
+            PrintTriangle(matrix);
+        }
+
+        private static void PrintTriangle(long[][] matrix)
+        {
+            for (int row = 0; row < matrix.Length; row++)
+            {
+                for (int col = 0; col < matrix[row].Length; col++)
+                {
+                    Console.Write(matrix[row][col] + " ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
