25e290780c887d11c5c7d4228abe3098db75c85e
vasil tatarov <vasil.tatarov@abv.bg>
2020-09-21 23:01:18 +0300

    Multidimension

---
+++ C# Advanced/Multidimensional Arrays/Multidimensional-Arrays-Exercise/P06_JaggedArrayManipulator/StartUp.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Linq;
+
+namespace P06_JaggedArrayManipulator
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var jagged = new double[n][];
+            ReadValues(jagged);
+            AnalyzingJagged(jagged);
+
+            while (true)
+            {
+                var command = Console.ReadLine();
+
+                if (command == "End")
+                {
+                    break;
+                }
+
+                var args = command.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                if (args[0] == "Add")
+                {
+                    var row = int.Parse(args[1]);
+                    var col = int.Parse(args[2]);
+                    var value = int.Parse(args[3]);
+
+                    if (ValidateIndex(row, col, jagged))
+                    {
+                        jagged[row][col] += value;
+                    }
+                }
+                else if (args[0] == "Subtract")
+                {
+                    var row = int.Parse(args[1]);
+                    var col = int.Parse(args[2]);
+                    var value = int.Parse(args[3]);
+
+                    if (ValidateIndex(row, col, jagged))
+                    {
+                        jagged[row][col] -= value;
+                    }
+                }
+            }
+
+            PrintJagged(jagged);
+        }
+
+        private static bool ValidateIndex(int row, int col, double[][] jagged)
+            => row >= 0 && row < jagged.Length && col >= 0 && col < jagged[row].Length;
+
+        private static void AnalyzingJagged(double[][] jagged)
+        {
+            for (int i = 0; i < jagged.Length - 1; i++)
+            {
+                if (jagged[i].Length == jagged[i + 1].Length)
+                {
+                    jagged[i] = jagged[i].Select(x => x * 2).ToArray();
+                    jagged[i + 1] = jagged[i + 1].Select(x => x * 2).ToArray();
+                }
+                else
+                {
+                    jagged[i] = jagged[i].Select(x => x / 2).ToArray();
+                    jagged[i + 1] = jagged[i + 1].Select(x => x / 2).ToArray();
+                }
+            }
+        }
+
+        private static void PrintJagged(double[][] jagged)
+        {
+            foreach (var row in jagged)
+            {
+                Console.WriteLine(string.Join(" ", row));
+            }
+        }
+
+        private static void ReadValues(double[][] jagged)
+        {
+            for (int row = 0; row < jagged.Length; row++)
+            {
+                var sequence = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(double.Parse).ToArray();
+                jagged[row] = sequence;
+            }
+        }
+    }
+}
---
+++ C# Advanced/Multidimensional Arrays/Multidimensional-Arrays-Exercise/P08_Bombs/StartUp.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Linq;
+
+namespace P08_Bombs
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var matrix = ReadMatrix(n);
+            var coordinates = Console.ReadLine().Split();
+
+            for (int i = 0; i < coordinates.Length; i++)
+            {
+                var args = coordinates[i].Split(",", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+                var row = args[0];
+                var col = args[1];
+
+                if (matrix[row, col] <= 0)
+                {
+                    continue;
+                }
+
+                var bombValue = matrix[row, col];
+                matrix[row, col] = 0;
+                BombExplode(matrix, row, col, bombValue);
+            }
+
+            var aliveCells = 0;
+            var sumOfCells = 0;
+            FindAliveCells(matrix, ref aliveCells, ref sumOfCells);
+
+            Console.WriteLine($"Alive cells: {aliveCells}");
+            Console.WriteLine($"Sum: {sumOfCells}");
+            PrintMatrix(matrix);
+        }
+
+        private static void FindAliveCells(int[,] matrix, ref int aliveCells, ref int sumOfCells)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    if (matrix[row, col] > 0)
+                    {
+                        aliveCells++;
+                        sumOfCells += matrix[row, col];
+                    }
+                }
+            }
+        }
+
+        private static void BombExplode(int[,] matrix, int row, int col, int bombValue)
+        {
+            if (IsValidIndex(row - 1, col - 1, matrix) && ValueIsBiggerThanZero(row - 1, col - 1, matrix))
+            {
+                matrix[row - 1, col - 1] -= bombValue;
+            }
+            if (IsValidIndex(row - 1, col, matrix) && ValueIsBiggerThanZero(row - 1, col, matrix))
+            {
+                matrix[row - 1, col] -= bombValue;
+            }
+            if (IsValidIndex(row - 1, col + 1, matrix) && ValueIsBiggerThanZero(row - 1, col + 1, matrix))
+            {
+                matrix[row - 1, col + 1] -= bombValue;
+            }
+            if (IsValidIndex(row, col - 1, matrix) && ValueIsBiggerThanZero(row, col - 1, matrix))
+            {
+                matrix[row, col - 1] -= bombValue;
+            }
+            if (IsValidIndex(row, col + 1, matrix) && ValueIsBiggerThanZero(row, col + 1, matrix))
+            {
+                matrix[row, col + 1] -= bombValue;
+            }
+            if (IsValidIndex(row + 1, col - 1, matrix) && ValueIsBiggerThanZero(row + 1, col - 1, matrix))
+            {
+                matrix[row + 1, col - 1] -= bombValue;
+            }
+            if (IsValidIndex(row + 1, col, matrix) && ValueIsBiggerThanZero(row + 1, col, matrix))
+            {
+                matrix[row + 1, col] -= bombValue;
+            }
+            if (IsValidIndex(row + 1, col + 1, matrix) && ValueIsBiggerThanZero(row + 1, col + 1, matrix))
+            {
+                matrix[row + 1, col + 1] -= bombValue;
+            }
+        }
+
+        private static bool ValueIsBiggerThanZero(int row, int col, int[,] matrix)
+            => matrix[row, col] > 0;
+
+        private static bool IsValidIndex(int row, int col, int[,] matrix)
+            => row >= 0 && row < matrix.GetLength(0) && col >= 0 && col < matrix.GetLength(1);
+
+        private static void PrintMatrix(int[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write(matrix[row, col] + " ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        private static int[,] ReadMatrix(int n)
+        {
+            var matrix = new int[n, n];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                var values = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = values[col];
+                }
+            }
+
+            return matrix;
+        }
+    }
+}
