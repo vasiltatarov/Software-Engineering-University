51205cb55c8bf8d376b8d82f97c67e543e51dce1
vasil tatarov <vasil.tatarov@abv.bg>
2020-10-01 23:17:53 +0300

    Create 01.OddLines.cs

---
+++ C# Advanced/Streams, Files and Directories/Lab/01.OddLines.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+
+namespace _01.OddLines
+{
+    public class Program
+    {
+        static void Main()
+        {
+            var reader = new StreamReader(@"..\..\..\Input.txt"); // Create text file, if it doesnt exist!
+            var line = reader.ReadLine();
+            var counter = 0;
+            var writer = new StreamWriter(@"..\..\..\Output.txt");
+
+            using (writer)
+            {
+                while (line != null)
+                {
+                    if (counter % 2 != 0)
+                    {
+                        writer.WriteLine(line);
+                    }
+
+                    counter++;
+                    line = reader.ReadLine();
+                }
+            }
+        }
+    }
+}
