0e9a177c3c09f023c7e12b7f3dab2f17d7810b00
vasil tatarov <vasil.tatarov@abv.bg>
2020-10-05 14:28:34 +0300

    Stream,File,Directory

---
+++ C# Advanced/Streams, Files and Directories/Exercise/Full_Directory_Traversal.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace Full_Directory_Traversal
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            var path = Console.ReadLine();  //Example:  "D:\GitHubRepos\Software-Engineering-University\C# Advanced"
+            var directories = Directory.GetDirectories(path);
+
+            var allFilesAndDirectories = new StringBuilder();
+
+            TraverseDirectories(directories, 2, allFilesAndDirectories);
+
+            Console.WriteLine(allFilesAndDirectories.ToString().TrimEnd());
+        }
+
+        private static void TraverseDirectories(string[] directories, int indent, StringBuilder allFilesAndDirectories)
+        {
+            foreach (var directory in directories)
+            {
+                var currentDirectory = Directory.GetDirectories(directory);
+
+                var dir = new FileInfo(directory);
+                allFilesAndDirectories.AppendLine($"Dir:{new string('-', indent)}{dir.Name}");
+
+                var files = Directory.GetFiles(directory);
+
+                foreach (var file in files)
+                {
+                    var fileInfo = new FileInfo(file);
+                    allFilesAndDirectories.AppendLine($"File:{new string('-', indent)}{fileInfo.Name}");
+                }
+
+                allFilesAndDirectories.AppendLine();
+
+                TraverseDirectories(currentDirectory, indent + 2, allFilesAndDirectories);
+            }
+        }
+    }
+}
