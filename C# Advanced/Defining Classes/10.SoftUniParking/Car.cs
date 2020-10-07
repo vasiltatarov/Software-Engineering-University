e7a5de8ee4450a7060fd258e4c706b77835ad4d7
vasil tatarov <vasil.tatarov@abv.bg>
2020-10-07 23:56:37 +0300

    OOP

---
+++ C# Advanced/Defining Classes/10.SoftUniParking/Car.cs
@@ -0,0 +1,85 @@
+﻿using System.Text;
+
+namespace SoftUniParking
+{
+    public class Car //: IComparable
+    {
+        private string make;
+        private string model;
+        private int horsePower;
+        private string registrationNumber;
+
+        public Car(string make, string model, int horsePower, string registrationNumber)
+        {
+            this.Make = make;
+            this.Model = model;
+            this.HorsePower = horsePower;
+            this.RegistrationNumber = registrationNumber;
+        }
+
+        public string Make
+        {
+            get
+            {
+                return this.make;
+            }
+            set
+            {
+                this.make = value;
+            }
+        }
+        public string Model
+        {
+            get
+            {
+                return this.model;
+            }
+            set
+            {
+                this.model = value;
+            }
+        }
+        public int HorsePower
+        {
+            get
+            {
+                return this.horsePower;
+            }
+            set
+            {
+                this.horsePower = value;
+            }
+        }
+        public string RegistrationNumber
+        {
+            get
+            {
+                return this.registrationNumber;
+            }
+            set
+            {
+                this.registrationNumber = value;
+            }
+        }
+
+        //public int CompareTo(object obj)
+        //{
+        //    var current = (Car)obj;
+
+        //    return this.RegistrationNumber.CompareTo(current.RegistrationNumber);
+        //}
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+
+            sb
+                .AppendLine($"Make: {this.Make}")
+                .AppendLine($"Model: {this.Model}")
+                .AppendLine($"HorsePower: {this.HorsePower}")
+                .AppendLine($"RegistrationNumber: {this.RegistrationNumber}");
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ C# Advanced/Defining Classes/10.SoftUniParking/Parking.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace SoftUniParking
+{
+    public class Parking
+    {
+        private List<Car> data;
+        private int capacity;
+
+        public Parking(int capacity)
+        {
+            this.capacity = capacity;
+            this.data = new List<Car>();
+        }
+
+        public int Count => this.data.Count;
+
+        public string AddCar(Car car)
+        {
+            if (this.data.Any(x => x.RegistrationNumber == car.RegistrationNumber))
+            {
+                return "Car with that registration number, already exists!";
+            }
+
+            if (this.data.Count >= this.capacity)
+            {
+                return "Parking is full!";
+            }
+
+            this.data.Add(car);
+            return $"Successfully added new car {car.Make} {car.RegistrationNumber}";
+        }
+
+        public string RemoveCar(string registrationNumber)
+        {
+            Car found = null;
+
+            foreach (var car in this.data)
+            {
+                if (car.RegistrationNumber == registrationNumber)
+                {
+                    found = car;
+                }
+            }
+
+            if (found == null)
+            {
+                return "Car with that registration number, doesn't exist!";
+            }
+
+            this.data.Remove(found);
+            return $"Successfully removed {registrationNumber}";
+        }
+
+        public Car GetCar(string RegistrationNumber)
+        {
+            foreach (var car in this.data)
+            {
+                if (car.RegistrationNumber == RegistrationNumber)
+                {
+                    return car;
+                }
+            }
+
+            return null;
+        }
+
+        public void RemoveSetOfRegistrationNumber(List<string> registrationNumbers)
+        {
+            foreach (var registration in registrationNumbers)
+            {
+                this.RemoveCar(registration);
+            }
+        }
+    }
+}
