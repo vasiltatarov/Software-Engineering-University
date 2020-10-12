5da8321be850d46c3338e6db7da747cef80787ad
vasil tatarov <vasil.tatarov@abv.bg>
2020-10-12 17:53:25 +0300

    Solution

---
+++ C# Advanced/Iterators and Comparators/01.Library/Book.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace IteratorsAndComparators
+{
+    public class Book
+    {
+        public Book(string title, int year, params string[] authors)
+        {
+            this.Title = title;
+            this.Year = year;
+            this.Authors = new List<string>(authors);
+        }
+        
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public IReadOnlyList<string> Authors { get; set; }
+
+        public override string ToString()
+        {
+            return $"{this.Title} - {this.Year}";
+        }
+    }
+}
---
+++ C# Advanced/Iterators and Comparators/01.Library/Library.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace IteratorsAndComparators
+{
+    public class Library : IEnumerable<Book>
+    {
+        private List<Book> books;
+
+        public Library(params Book[] books)
+        {
+            this.books = new List<Book>(books);
+        }
+
+        public IEnumerator<Book> GetEnumerator()
+        {
+            this.books.Sort(new BookComparator());
+
+            return new LibraryIterator(this.books);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+            => this.GetEnumerator();
+
+        private class LibraryIterator : IEnumerator<Book>
+        {
+            private List<Book> books;
+            private int currentIndex;
+
+            public LibraryIterator(IEnumerable<Book> books)
+            {
+                this.Reset();
+                this.books = new List<Book>(books);
+            }
+
+            public Book Current => this.books[this.currentIndex];
+
+            object IEnumerator.Current => this.Current;
+
+            public void Dispose()
+            {
+            }
+
+            public bool MoveNext() => ++this.currentIndex < this.books.Count;
+
+            public void Reset()
+            {
+                this.currentIndex = -1;
+            }
+        }
+    }
+}
