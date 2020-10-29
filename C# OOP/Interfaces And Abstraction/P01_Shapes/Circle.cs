08359061fb5c708dd85947b25fa286965dd7c009
vasil tatarov <vasil.tatarov@abv.bg>
2020-10-30 00:20:38 +0200

    HomeWork

---
+++ C# OOP/Interfaces And Abstraction/P01_Shapes/Circle.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+
+namespace Shapes
+{
+    public class Circle : IDrawable
+    {
+        private int radius;
+        public Circle(int radius)
+        {
+            this.radius = radius;
+        }
+
+        public string Draw()
+        {
+            var sb = new StringBuilder();
+            var rIn = this.radius - 0.4d;
+            var rOut = this.radius + 0.4d;
+
+            for (double y = this.radius; y >= -this.radius; --y)
+            {
+                for (double x = -this.radius; x < rOut; x += 0.5)
+                {
+                    var value = (x * x) + (y * y);
+
+                    if (value >= rIn * rIn && value <= rOut * rOut)
+                    {
+                        sb.Append('*');
+                    }
+                    else
+                    {
+                        sb.Append(' ');
+                    }
+                }
+
+                sb.AppendLine();
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ C# OOP/Interfaces And Abstraction/P01_Shapes/Rectangle.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+
+namespace Shapes
+{
+    public class Rectangle : IDrawable
+    {
+        private int width;
+        private int height;
+
+        public Rectangle(int width, int height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+
+        public string Draw()
+        {
+            var sb = new StringBuilder();
+            this.DrawLine(this.width, '*', '*', sb);
+
+            for (int i = 1; i < this.height - 1; ++i)
+            {
+                this.DrawLine(this.width, '*', ' ', sb);
+            }
+
+            this.DrawLine(this.width, '*', '*', sb);
+
+            return sb.ToString().TrimEnd();
+        }
+
+        private void DrawLine(int width, char end, char mid, StringBuilder sb)
+        {
+            sb.Append(end);
+
+            for (int i = 1; i < width - 1; ++i)
+            {
+                sb.Append(mid);
+            }
+
+            sb.AppendLine(end.ToString());
+        }
+    }
+}
