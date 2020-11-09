2e7054e601cde63239242c5b01e097caa7054373
vasil tatarov <vasil.tatarov@abv.bg>
2020-11-10 01:15:21 +0200

    homework

---
+++ C# OOP/Reflection and Attributes/P02_Attributes/AuthorAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AuthorProblem
+{
+    public class AuthorAttribute : Attribute
+    {
+        public AuthorAttribute(string name)
+        {
+            this.Name = name;
+        }
+
+        public string Name { get; set; }  
+    }
+}
---
+++ C# OOP/Reflection and Attributes/P02_Attributes/StartUp.cs
@@ -0,0 +1,13 @@
+﻿namespace AuthorProblem
+{
+    [Author("Ventsi")]
+    public class StartUp
+    {
+        [Author("Gosho")]
+        public static void Main()
+        {
+            var tracker = new Tracker();
+            tracker.PrintMethodsByAuthor();
+        }
+    }
+}
