b63d1a6169906be4b6337b2c280b05fd98c4195c
vasil tatarov <vasil.tatarov@abv.bg>
2020-12-07 12:47:01 +0200

    graphs

---
+++ Algorithms Fundamentals (with C#)/Graph Theory, Traversal and Shortest Paths/P02_SourceRemovalTopologicalSorting/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P02_SourceRemovalTopologicalSorting
+{
+    class Program
+    {
+        private static Dictionary<string, List<string>> graph;
+        private static Dictionary<string, int> dependencies;
+
+        static void Main()
+        {
+            try
+            {
+                var n = int.Parse(Console.ReadLine());
+
+                graph = ReadGraph(n);
+                dependencies = ExtractDependencies();
+                var sorted = TopologicalSort();
+
+                Console.WriteLine($"Topological sorting: {string.Join(", ", sorted)}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private static List<string> TopologicalSort()
+        {
+            var sorted = new List<string>();
+
+            while (dependencies.Count > 0)
+            {
+                var node = dependencies.FirstOrDefault(x => x.Value == 0);
+
+                if (node.Key == null)
+                {
+                    break;
+                }
+
+                var key = node.Key;
+                var children = graph[node.Key];
+
+                sorted.Add(key);
+
+                foreach (var child in children)
+                {
+                    dependencies[child]--;
+                }
+
+                dependencies.Remove(key);
+            }
+
+            if (dependencies.Count > 0)
+            {
+                throw new InvalidOperationException("Invalid topological sorting");
+            }
+
+            return sorted;
+        }
+
+        private static Dictionary<string, int> ExtractDependencies()
+        {
+            var dependenciesResult = new Dictionary<string, int>();
+
+            foreach (var (key, children) in graph)
+            {
+                if (!dependenciesResult.ContainsKey(key))
+                {
+                    dependenciesResult.Add(key, 0);
+                }
+
+                foreach (var child in children)
+                {
+                    if (!dependenciesResult.ContainsKey(child))
+                    {
+                        dependenciesResult.Add(child, 0);
+                    }
+
+                    dependenciesResult[child]++;
+                }
+            }
+
+            return dependenciesResult;
+        }
+
+        private static Dictionary<string, List<string>> ReadGraph(int n)
+        {
+            var result = new Dictionary<string, List<string>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                var line = Console.ReadLine().Split(new string[] { "->", ",", " " }, StringSplitOptions.RemoveEmptyEntries);
+                var key = line[0];
+                var childrens = line.Skip(1).ToList();
+
+                result.Add(key, new List<string>(childrens));
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Algorithms Fundamentals (with C#)/Graph Theory, Traversal and Shortest Paths/P03_ShortestPath/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P03_ShortestPath
+{
+    class Program
+    {
+        private static List<int>[] graph;
+        private static bool[] visited;
+        private static int[] parents;
+
+        static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var edges = int.Parse(Console.ReadLine());
+
+            ReadGraph(n, edges);
+            visited = new bool[graph.Length];
+            parents = new int[graph.Length];
+            Array.Fill(parents, -1);
+
+            var source = int.Parse(Console.ReadLine());
+            var destination = int.Parse(Console.ReadLine());
+            
+            Bfs(source, destination);
+        }
+
+        private static void Bfs(int startNode, int destination)
+        {
+            if (visited[startNode])
+            {
+                return;
+            }
+
+            var queue = new Queue<int>();
+
+            queue.Enqueue(startNode);
+            visited[startNode] = true;
+
+            while (queue.Any())
+            {
+                var node = queue.Dequeue();
+
+                if (node == destination)
+                {
+                    var path = ReconstructGraph(destination);
+
+                    Console.WriteLine($"Shortest path length is: {path.Count - 1}");
+                    Console.WriteLine(string.Join(" ", path));
+
+                    return;
+                }
+
+                if (graph[node] == null)
+                {
+                    continue;
+                }
+
+                foreach (var child in graph[node])
+                {
+                    if (!visited[child])
+                    {
+                        parents[child] = node;
+                        queue.Enqueue(child);
+                        visited[child] = true;
+                    }
+                }
+            }
+        }
+
+        private static Stack<int> ReconstructGraph(int destination)
+        {
+            var result = new Stack<int>();
+            var index = destination;
+
+            while (index > -1)
+            {
+                result.Push(index);
+                index = parents[index];
+            }
+
+            return result;
+        }
+
+        private static void ReadGraph(int n, int edges)
+        {
+            graph = new List<int>[n + 1];
+
+            for (int i = 0; i < edges; i++)
+            {
+                var edge = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+                var from = edge[0];
+                var to = edge[1];
+
+                if (graph[from] == null)
+                {
+                    graph[from] = new List<int>();
+                }
+
+                graph[from].Add(to);
+            }
+        }
+    }
+}
