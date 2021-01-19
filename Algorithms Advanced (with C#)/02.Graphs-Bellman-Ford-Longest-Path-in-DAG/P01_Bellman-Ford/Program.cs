1797a27fc79e944f781b5448a6f3be3bdd92f294
vasil tatarov <vasil.tatarov@abv.bg>
2021-01-19 14:16:19 +0200

    LongestPathInDAG

---
+++ Algorithms Advanced (with C#)/02.Graphs-Bellman-Ford-Longest-Path-in-DAG/P01_Bellman-Ford/Program.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P01_Bellman_Ford
+{
+    public class Edge
+    {
+        public int Source { get; set; }
+
+        public int Destination { get; set; }
+
+        public int Weight { get; set; }
+    }
+
+    public class Program
+    {
+        private static List<Edge> edges;
+        private static double shortestPath;
+
+        public static void Main(string[] args)
+        {
+            var nodes = int.Parse(Console.ReadLine());
+            var edgesCount = int.Parse(Console.ReadLine());
+
+            edges = ReadGraph(edgesCount);
+
+            var source = int.Parse(Console.ReadLine());
+            var destination = int.Parse(Console.ReadLine());
+
+            try
+            {
+                var path = FindShortestPath(nodes, source, destination);
+                Console.WriteLine(string.Join(" ", path));
+                Console.WriteLine(shortestPath);
+            }
+            catch (Exception ioe)
+            {
+                Console.WriteLine(ioe.Message);
+                return;
+            }
+        }
+
+        private static Stack<int> FindShortestPath(int nodes, int source, int destination)
+        {
+            var distances = new double[nodes + 1];
+            Array.Fill(distances, double.PositiveInfinity);
+            distances[source] = 0;
+
+            var prev = new int[nodes + 1];
+            Array.Fill(prev, -1);
+
+            for (int i = 0; i < nodes - 1; i++)
+            {
+                var isUpdate = false;
+
+                foreach (var edge in edges)
+                {
+                    if (double.IsPositiveInfinity(edge.Source))
+                    {
+                        continue;
+                    }
+
+                    var newDistance = distances[edge.Source] + edge.Weight;
+
+                    if (newDistance < distances[edge.Destination])
+                    {
+                        distances[edge.Destination] = newDistance;
+                        prev[edge.Destination] = edge.Source;
+
+                        isUpdate = true;
+                    }
+                }
+
+                if (!isUpdate)
+                {
+                    break;
+                }
+            }
+
+            foreach (var edge in edges)
+            {
+                if (double.IsPositiveInfinity(edge.Source))
+                {
+                    continue;
+                }
+
+                var newDistance = distances[edge.Source] + edge.Weight;
+
+                if (newDistance < distances[edge.Destination])
+                {
+                    throw new InvalidOperationException($"Negative Cycle Detected");
+                }
+            }
+
+            var path = ReconstructPath(prev, destination);
+            shortestPath = distances[destination];
+
+            return path;
+        }
+
+        private static Stack<int> ReconstructPath(int[] prev, int node)
+        {
+            var path = new Stack<int>();
+
+            while (node != -1)
+            {
+                path.Push(node);
+                node = prev[node];
+            }
+
+            return path;
+        }
+
+        private static List<Edge> ReadGraph(int edgesCount)
+        {
+            var result = new List<Edge>();
+
+            for (int i = 0; i < edgesCount; i++)
+            {
+                var input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                var source = input[0];
+                var destination = input[1];
+                var weight = input[2];
+
+                result.Add(new Edge()
+                {
+                    Source = source,
+                    Destination = destination,
+                    Weight = weight,
+                });
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Algorithms Advanced (with C#)/02.Graphs-Bellman-Ford-Longest-Path-in-DAG/P02_LongestPath/Program.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P02_LongestPath
+{
+    public class Edge
+    {
+        public int Source { get; set; }
+
+        public int Destination { get; set; }
+
+        public int Weight { get; set; }
+    }
+
+    class Program
+    {
+        private static List<Edge>[] graph;
+
+        static void Main(string[] args)
+        {
+            var nodes = int.Parse(Console.ReadLine());
+            var edgesCount = int.Parse(Console.ReadLine());
+
+            graph = ReadGraph(nodes, edgesCount);
+
+            var source = int.Parse(Console.ReadLine());
+            var destination = int.Parse(Console.ReadLine());
+
+            var path = GetLongestPath(source, destination);
+
+            Console.WriteLine(string.Join(" ", path));
+        }
+
+        /// <summary>
+        /// If you wanna to get shortest path with this algorithm
+        /// Step 1 - Array.Fill(distances, double.PositiveInfinity);
+        /// Step 2 - if (newDistance < distances[edge.Destination])
+        /// </summary>
+        private static Stack<int> GetLongestPath(int source, int destination)
+        {
+            var sortedNodes = TopologicalSorting();
+
+            var distances = new double[graph.Length];
+            Array.Fill(distances, double.NegativeInfinity);
+            distances[source] = 0;
+
+            var prev = new int[graph.Length];
+            Array.Fill(prev, -1);
+
+            while (sortedNodes.Count > 0)
+            {
+                var node = sortedNodes.Pop();
+
+                foreach (var edge in graph[node])
+                {
+                    var newDistance = distances[node] + edge.Weight;
+                    if (newDistance > distances[edge.Destination])
+                    {
+                        distances[edge.Destination] = newDistance;
+                        prev[edge.Destination] = node;
+                    }
+                }
+            }
+
+            Console.WriteLine(distances[destination]);
+
+            var path = ReconstructPath(prev, destination);
+
+            return path;
+        }
+
+        private static Stack<int> ReconstructPath(int[] prev, int node)
+        {
+            var path = new Stack<int>();
+
+            while (node != -1)
+            {
+                if (path.Contains(node))
+                {
+                    return path;
+                }
+
+                path.Push(node);
+                node = prev[node];
+            }
+
+            return path;
+        }
+
+        private static Stack<int> TopologicalSorting()
+        {
+            var visited = new bool[graph.Length];
+            var sorted = new Stack<int>();
+
+            for (int node = 1; node < graph.Length; node++)
+            {
+                DFS(node, visited, sorted);
+            }
+
+            return sorted;
+        }
+
+        private static void DFS(int node, bool[] visited, Stack<int> sorted)
+        {
+            if (visited[node])
+            {
+                return;
+            }
+
+            visited[node] = true;
+
+            foreach (var edge in graph[node])
+            {
+                DFS(edge.Destination, visited, sorted);
+            }
+
+            sorted.Push(node);
+        }
+
+        private static List<Edge>[] ReadGraph(int nodes, int edgesCount)
+        {
+            var result = new List<Edge>[nodes + 1];
+
+            for (int node = 0; node < result.Length; node++)
+            {
+                result[node] = new List<Edge>();
+            }
+
+            for (int i = 0; i < edgesCount; i++)
+            {
+                var input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                var source = input[0];
+                var destination = input[1];
+                var weight = input[2];
+
+                result[source].Add(new Edge()
+                {
+                    Source = source,
+                    Destination = destination,
+                    Weight = weight,
+                });
+            }
+
+            return result;
+        }
+    }
+}
