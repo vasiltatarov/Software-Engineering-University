bed98cebcf86f65062ca37868f1f0737fe77d1c7
vasiltatarov <vasiltatarov3@gmail.com>
2021-03-05 18:38:39 +0200

    Auto Mapper

---
+++ Entity Framework Core/07. Auto-Mapping-Objects/FastFood.Core/Controllers/EmployeesController.cs
@@ -0,0 +1,58 @@
+﻿using System.Linq;
+using AutoMapper.QueryableExtensions;
+using FastFood.Models;
+
+namespace FastFood.Core.Controllers
+{
+    using System;
+    using AutoMapper;
+    using Data;
+    using Microsoft.AspNetCore.Mvc;
+    using ViewModels.Employees;
+
+    public class EmployeesController : Controller
+    {
+        private readonly FastFoodContext context;
+        private readonly IMapper mapper;
+
+        public EmployeesController(FastFoodContext context, IMapper mapper)
+        {
+            this.context = context;
+            this.mapper = mapper;
+        }
+
+        public IActionResult Register()
+        {
+            var employeeViewModel = context.Positions
+                .ProjectTo<RegisterEmployeeViewModel>(this.mapper.ConfigurationProvider)
+                .ToList();
+
+            return this.View(employeeViewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Register(RegisterEmployeeInputModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+
+            var employee = this.mapper.Map<Employee>(model);
+
+            this.context.Employees.Add(employee);
+            this.context.SaveChanges();
+
+            return RedirectToAction("All", "Employees");
+        }
+
+        public IActionResult All()
+        {
+            var employees = this.context.Employees
+                .ProjectTo<EmployeesAllViewModel>(this.mapper.ConfigurationProvider)
+                .ToList();
+
+            return this.View(employees);
+        }
+    }
+}
---
+++ Entity Framework Core/07. Auto-Mapping-Objects/FastFood.Core/ViewModels/Items/CreateItemInputModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FastFood.Core.ViewModels.Items
+{
+    public class CreateItemInputModel
+    {
+        [StringLength(30, MinimumLength = 3)]
+        public string Name { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+
+        [Required]
+        public int CategoryId { get; set; }
+    }
+}
