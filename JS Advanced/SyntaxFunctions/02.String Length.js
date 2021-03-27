ea172dba804e244ed4ee88918f224b034c354bcd
vasiltatarov <vasiltatarov3@gmail.com>
2021-03-27 11:28:33 +0200

    first step in JS Advanced

---
+++ JS Advanced/SyntaxFunctions/02.String Length.js
@@ -0,0 +1,7 @@
+function stringsLength(str1, str2, str3){
+    let result = str1.length + str2.length + str3.length; 
+    console.log(result);
+    console.log(Math.floor(result / 3));
+}
+
+stringsLength('pasta', '5', '22.3');
